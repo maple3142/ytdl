46645fef2986e44f79467f6b3dc1fccf0cd62b4c
maple3142 <kirby741852963@gmail.com>
2018-04-20 22:14:23 +0800

    add server

---
+++ index.js
@@ -0,0 +1,32 @@
+const Koa = require('koa')
+const app = new Koa()
+const getVideo = require('./getVideo')
+
+app.use(async (ctx, next) => {
+	const start = Date.now()
+	await next()
+	const ms = Date.now() - start
+	ctx.set('X-Response-Time', `${ms}ms`)
+})
+
+app.use(async (ctx, next) => {
+	const start = Date.now()
+	await next()
+	const ms = Date.now() - start
+	console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+app.use(async ctx => {
+	const { id } = ctx.request.query
+	if (!id) {
+		ctx.throw(400, 'id required')
+	}
+	try {
+		ctx.body = await getVideo(id)
+	} catch (e) {
+		ctx.body = e
+	}
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`listen on: http://localhost:${PORT}`))

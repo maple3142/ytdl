e2f62e05c688572fa2e92c467865ccf08173a3be
maple3142 <kirby741852963@gmail.com>
2018-10-04 23:44:43 +0800

    add tg bot play

--- tgbot.js
+++ tgbot.js
@@ -2,11 +2,14 @@
 process.env.NTBA_FIX_319 = 1
 
 const getUrls = require('get-urls')
-const xf = require('xfetch-js')
+const cache = require('memory-cache')
+const uuidv4 = require('uuid/v4')
 const TelegramBot = require('node-telegram-bot-api')
 const getVideo = require('./getvid')
 const getBestThumbnail = require('./thumbnail')
 const bot = new TelegramBot(process.env.TG_TOKEN)
+
+const AN_HOUR = 1000 * 60 * 60
 
 const parseId = url => {
 	try {
@@ -37,6 +40,25 @@ bot.on('text', async msg => {
 			'Send a YouTube video URL to me, and I will retrieve raw video URL for you.'
 		)
 	}
+	if (/^\/play/.test(msg.text)) {
+		const id = msg.text.slice(6) //strip
+		const url = cache.get(id)
+		if (!url) {
+			return bot.sendMessage(
+				msg.chat.id,
+				'No video matching the id are found!\nMaybe it has been deleted from cache.'
+			)
+		} else {
+			console.info(`${id} -> ${url}`)
+			return bot.sendVideo(msg.chat.id, url).catch(() =>
+				bot.sendMessage(
+					msg.chat.id,
+					`Failed to send video! Please try the others.
+Possible reasons: Telegram doesn\'t support this filetype or the video to too big.`
+				)
+			)
+		}
+	}
 	console.info(msg)
 	const urls = getUrls(msg.text)
 	let validUrlCnt = 0
@@ -44,22 +66,47 @@ bot.on('text', async msg => {
 		try {
 			const id = parseId(url)
 			const { stream, adaptive, meta } = await getVideo(id)
+
+			// add video uuid
+			for (const s of stream) {
+				s.uuid = uuidv4().replace(/-/g, '_')
+				cache.put(s.uuid, s.url, AN_HOUR)
+			}
+			for (const s of adaptive) {
+				s.uuid = uuidv4().replace(/-/g, '_')
+				cache.put(s.uuid, s.url, AN_HOUR)
+			}
+
 			const thumbnail = await getBestThumbnail(meta.thumbnail_url)
 			const { message_id: photomsgid } = await bot.sendPhoto(msg.chat.id, thumbnail, {
 				caption: `[${meta.title}](https://www.youtube.com/watch?v=${id})`,
 				parse_mode: 'Markdown'
 			})
-			const sstr = '**Stream**\n' + stream.map(s => `[${s.quality}](${s.url})`).join('\n')
+			const sstr =
+				'**Stream**\n' +
+				stream.map(s => `[${s.quality}](${s.url}) /play\\_${s.uuid.replace(/_/g, '\\_')}`).join('\n\n')
 			const astr =
 				'**Adaptive**\n' +
-				adaptive.map(s => `[${(s.quality_label ? s.quality_label + ':' : '') + s.type}](${s.url})`).join('\n')
-			await bot.sendMessage(msg.chat.id, `Video **${id}**\n` + sstr + '\n' + astr, {
+				adaptive
+					.map(
+						s =>
+							`[${(s.quality_label ? s.quality_label + ':' : '') + s.type}](${
+								s.url
+							}) /play\\_${s.uuid.replace(/_/g, '\\_')}`
+					)
+					.join('\n\n')
+			await bot.sendMessage(msg.chat.id, sstr, {
+				parse_mode: 'Markdown',
+				reply_to_message_id: photomsgid
+			})
+			await bot.sendMessage(msg.chat.id, astr, {
 				parse_mode: 'Markdown',
 				reply_to_message_id: photomsgid
 			})
 			validUrlCnt++
 		} catch (e) {
 			// invalid url
+			console.info(e)
 		}
 	}
 	if (validUrlCnt === 0) {

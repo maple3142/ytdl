56f498eaea1ea7d3a2b1749b28fe9bc7217c8a51
maple3142 <kirby741852963@gmail.com>
2019-02-07 18:40:54 +0800

    fix decsig

--- decsig.js
+++ decsig.js
@@ -1,34 +1,30 @@
 const xf = require('xfetch-js')
 const { createSafeFn, runInContext } = require('./sandboxutil')
 
-const fallback = a => {
-	// 20180916
-	var IK = {
-		jd: function(a) {
-			a.reverse()
-		},
-		VC: function(a, b) {
+const fallback = function anonymous(a) {
+	// 20190207
+	var Dv = {
+		Ed: function(a, b) {
 			a.splice(0, b)
 		},
-		uy: function(a, b) {
+		SR: function(a) {
+			a.reverse()
+		},
+		Q3: function(a, b) {
 			var c = a[0]
 			a[0] = a[b % a.length]
 			a[b % a.length] = c
 		}
 	}
 	a = a.split('')
-	IK.VC(a, 2)
-	IK.uy(a, 8)
-	IK.jd(a, 10)
-	IK.uy(a, 43)
-	IK.VC(a, 2)
-	IK.uy(a, 11)
-	IK.jd(a, 33)
+	Dv.Ed(a, 1)
+	Dv.Q3(a, 55)
+	Dv.Q3(a, 49)
 	return a.join('')
 }
 
 const parsedecsig = data => {
-	const fnnameresult = /yt\.akamaized\.net.*encodeURIComponent\)\((\w+)/.exec(data)
+	const fnnameresult = /yt\.akamaized\.net.*encodeURIComponent\((\w+)/.exec(data)
 	const fnname = fnnameresult[1]
 	const _argnamefnbodyresult = new RegExp(fnname + '=function\\((.+?)\\){(.+?)}').exec(data)
 	const [_, argname, fnbody] = _argnamefnbodyresult
@@ -46,7 +42,7 @@ module.exports = (id, safe = true) =>
 			const d = /<script >(var ytplayer[\s\S]*?)<\/script>/.exec(data)
 			const window = {}
 			// This script will throw an error if no window is provided
-			runInContext(d[1] + ';window.ytplayer = ytplayer', { window }) 
+			runInContext(d[1] + ';window.ytplayer = ytplayer', { window })
 			return xf.get('https://youtube.com' + window.ytplayer.config.assets.js).text()
 		})
 		.then(data => parsedecsig(data))

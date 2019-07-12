60cbf64b42fd31691350b6e428ac2b99dd358b4f
maple3142 <kirby741852963@gmail.com>
2019-07-12 19:46:37 +0800

    add ratelimit

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ const Koa = require('koa')
 const koaBody = require('koa-body')
 const mount = require('koa-mount')
 const graphqlHTTP = require('koa-graphql')
+const rateLimit = require('koa-ratelimit')
+const Redis = require('ioredis')
 const app = new Koa()
 const getVideo = require('./getvid')
 const gql = require('./gql')
 
 app.use(koaBody())
+
+// Ratelimit, prevent someone from abusing the demo site
+app.use(
+	ratelimit({
+		db: new Redis(),
+		duration: 3600000,
+		errorMessage: 'Too much requests.',
+		id: ctx => ctx.request.headers['x-forwarded-for'].split(',')[0], // get real ipv4 on glitch.com
+		headers: {
+			remaining: 'Rate-Limit-Remaining',
+			reset: 'Rate-Limit-Reset',
+			total: 'Rate-Limit-Total'
+		},
+		max: 60,
+		disableHeader: false
+	})
+)
 
 // cors
 app.use(async (ctx, next) => {

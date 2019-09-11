1b043776fea679e68efeec8adeae88deafc6e63b
maple3142 <kirby741852963@gmail.com>
2019-09-11 23:45:38 +0800

    fix

--- getvid.js
+++ getvid.js
@@ -1,36 +1,40 @@
 const xf = require('xfetch-js')
 const getdecsig = require('./decsig')
-const getBestThumbnail = require('./thumbnail')
 const qs = require('qs')
 
-const getVideo = id =>
-	xf
-		.get(`https://www.youtube.com/get_video_info`, { qs: { video_id: id, el: 'detailpage' } })
+const getVideo = async (id, decsig) => {
+	return xf
+		.get(`https://www.youtube.com/get_video_info?video_id=${id}&el=detailpage`)
 		.text()
 		.then(async data => {
 			const obj = qs.parse(data)
+			const playerResponse = JSON.parse(obj.player_response)
 			if (obj.status === 'fail') {
 				throw obj
 			}
 			const decsig = await getdecsig(id)
-			let stream = obj.url_encoded_fmt_stream_map.split(',').map(qs.parse)
-			if (stream[0].sp && stream[0].sp.includes('sig')) {
-				stream = stream
-					.map(x => ({ ...x, s: decsig(x.s) }))
-					.map(x => ({ ...x, url: x.url + `&sig=${x.s}` }))
+			let stream = []
+			if (playerResponse.streamingData.formats) {
+				stream = playerResponse.streamingData.formats.map(x => Object.assign(x, qs.parse(x.cipher)))
+				if (stream[0].sp && stream[0].sp.includes('sig')) {
+					stream = stream
+						.map(x => ({ ...x, s: decsig(x.s) }))
+						.map(x => ({ ...x, url: x.url + `&sig=${x.s}` }))
+				}
 			}
-			let adaptive = null
-			if (obj.adaptive_fmts) {
-				adaptive = obj.adaptive_fmts.split(',').map(qs.parse)
+
+			let adaptive = []
+			if (playerResponse.streamingData.adaptiveFormats) {
+				adaptive = playerResponse.streamingData.adaptiveFormats.map(x => Object.assign(x, qs.parse(x.cipher)))
 				if (adaptive[0].sp && adaptive[0].sp.includes('sig')) {
 					adaptive = adaptive
 						.map(x => ({ ...x, s: decsig(x.s) }))
 						.map(x => ({ ...x, url: x.url + `&sig=${x.s}` }))
 				}
 			}
-			obj.thumbnail_url = await getBestThumbnail(obj.thumbnail_url)
 			return { stream, adaptive, meta: obj }
 		})
+}
 module.exports = getVideo
 
 if (require.main === module) {

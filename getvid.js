834b63f2fb85534763cb5ceb73eb8f398f07aba3
maple3142 <kirby741852963@gmail.com>
2020-08-24 00:20:03 +0800

    fix get video

--- getvid.js
+++ getvid.js
@@ -3,45 +3,45 @@ const getdecsig = require('./decsig')
 const qs = require('qs')
 
 const getVideo = async id => {
-	return xf
+	const data = await xf
 		.get(
 			`https://www.youtube.com/get_video_info?video_id=${id}&el=detailpage`
 		)
 		.text()
-		.then(async data => {
-			const obj = qs.parse(data)
-			const playerResponse = JSON.parse(obj.player_response)
-			if (obj.status === 'fail') {
-				throw obj
+		.catch(err => null)
+	if (!data) return 'Adblock conflict'
+	const obj = qs.parse(data)
+	const playerResponse = JSON.parse(obj.player_response)
+	if (obj.status === 'fail') {
+		throw obj
+	}
+	const decsig = await getdecsig(id, false)
+	let stream = []
+	if (playerResponse.streamingData.formats) {
+		stream = playerResponse.streamingData.formats.map(x =>
+			Object.assign({}, x, qs.parse(x.cipher || x.signatureCipher))
+		)
+		if (stream[0].sp && stream[0].sp.includes('sig')) {
+			for (const obj of stream) {
+				obj.s = decsig(obj.s)
+				obj.url += `&sig=${obj.s}`
 			}
-			const decsig = await getdecsig(id, false)
-			let stream = []
-			if (playerResponse.streamingData.formats) {
-				stream = playerResponse.streamingData.formats.map(x =>
-					Object.assign(x, qs.parse(x.cipher))
-				)
-				if (stream[0].sp && stream[0].sp.includes('sig')) {
-					for (const obj of stream) {
-						obj.s = decsig(obj.s)
-						obj.url += `&sig=${obj.s}`
-					}
-				}
-			}
+		}
+	}
 
-			let adaptive = []
-			if (playerResponse.streamingData.adaptiveFormats) {
-				adaptive = playerResponse.streamingData.adaptiveFormats.map(x =>
-					Object.assign(x, qs.parse(x.cipher))
-				)
-				if (adaptive[0].sp && adaptive[0].sp.includes('sig')) {
-					for (const obj of adaptive) {
-						obj.s = decsig(obj.s)
-						obj.url += `&sig=${obj.s}`
-					}
-				}
+	let adaptive = []
+	if (playerResponse.streamingData.adaptiveFormats) {
+		adaptive = playerResponse.streamingData.adaptiveFormats.map(x =>
+			Object.assign({}, x, qs.parse(x.cipher || x.signatureCipher))
+		)
+		if (adaptive[0].sp && adaptive[0].sp.includes('sig')) {
+			for (const obj of adaptive) {
+				obj.s = decsig(obj.s)
+				obj.url += `&sig=${obj.s}`
 			}
-			return { stream, adaptive, meta: obj }
-		})
+		}
+	}
+	return { stream, adaptive, meta: obj, playerResponse }
 }
 module.exports = getVideo
 

27479ad9ec81c73a1d009e5b880e02166a4fd2a6
maple3142 <kirby741852963@gmail.com>
2021-02-04 02:41:12 +0800

    fix decsig

--- decsig.js
+++ decsig.js
@@ -2,31 +2,28 @@ const xf = require('xfetch-js')
 const { createSafeFn, runInContext } = require('./sandboxutil')
 
 const fallback = a => {
-	// 20200425
-	var Av = {
-		nt: function (a, b) {
+	// 20210204
+	var Cw = {
+		Ta: function (a, b) {
 			var c = a[0]
 			a[0] = a[b % a.length]
 			a[b % a.length] = c
 		},
-		xD: function (a) {
+		af: function (a) {
 			a.reverse()
 		},
-		J3: function (a, b) {
+		Hi: function (a, b) {
 			a.splice(0, b)
 		}
 	}
 	a = a.split('')
-	Av.nt(a, 17)
-	Av.nt(a, 16)
-	Av.xD(a, 4)
-	Av.nt(a, 25)
-	Av.xD(a, 63)
-	Av.nt(a, 50)
-	Av.nt(a, 35)
+	Cw.Hi(a, 2)
+	Cw.af(a, 41)
+	Cw.Hi(a, 1)
 	return a.join('')
 }
 
+const escapeRegExp = s => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
 const parseDecsig = data => {
 	if (data.startsWith('var script')) {
 		// they inject the script via script tag
@@ -38,41 +35,29 @@ const parseDecsig = data => {
 		eval(data)
 		data = obj.innerHTML
 	}
-	const fnNameResult = /=([a-zA-Z0-9\$]+?)\(decodeURIComponent/.exec(data)
-	const fnName = fnNameResult[1]
-	const _argNameFnBodyResult = new RegExp(
-		fnName + '=function\\((.+?)\\){(.+?)}'
-	).exec(data)
-	const [_, argName, fnBody] = _argNameFnBodyResult
-	const helperNameResult = /;(.+?)\..+?\(/.exec(fnBody)
-	const helperName = helperNameResult[1]
-	const helperResult = new RegExp(
-		'var ' + helperName + '={[\\s\\S]+?};'
-	).exec(data)
-	const helper = helperResult[0]
-	return new Function([argName], helper + '\n' + fnBody)
+	const fnnameresult = /=([a-zA-Z0-9\$]+?)\(decodeURIComponent/.exec(data)
+	const fnname = fnnameresult[1]
+	const _argnamefnbodyresult = new RegExp(escapeRegExp(fnname) + '=function\\((.+?)\\){(.+?)}').exec(data)
+	const [_, argname, fnbody] = _argnamefnbodyresult
+	const helpernameresult = /;(.+?)\..+?\(/.exec(fnbody)
+	const helpername = helpernameresult[1]
+	const helperresult = new RegExp('var ' + escapeRegExp(helpername) + '={[\\s\\S]+?};').exec(data)
+	const helper = helperresult[0]
+	return new Function([argname], helper + '\n' + fnbody)
 }
 module.exports = (id, safe = true) =>
 	xf
 		.get(`https://www.youtube.com/watch`, { qs: { v: id } })
 		.text()
 		.then(data => {
-			const d = /<script >(var ytplayer[\s\S]*?)<\/script>/.exec(data)
-			const window = {}
-			// This script will throw an error if no window is provided
-			runInContext(d[1] + ';var yt = window.ytplayer = ytplayer', {
-				window
-			})
-			return xf
-				.get('https://youtube.com' + window.ytplayer.config.assets.js)
-				.text()
+			const rg = /<script src="(\/.*?base\.js)"/
+			const basejs = rg.exec(data)[1]
+			return xf.get('https://youtube.com' + basejs).text()
 		})
 		.then(data => parseDecsig(data))
 		.then(fn => (safe ? createSafeFn(fn) : fn))
 		.catch(e => console.info('use fallback', e) || fallback)
 
 if (require.main === module) {
-	module
-		.exports('-tKVN2mAKRI', false)
-		.then(fn => console.log(fn.toString().split('\n').join('')))
+	module.exports('-tKVN2mAKRI', false).then(fn => console.log(fn.toString().split('\n').join('')))
 }
